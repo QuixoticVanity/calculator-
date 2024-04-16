1cced4ae7cc89f99158aac08927e7127826311c6
Debbie <deborahjlee01@gmail.com>
2024-04-15 17:31:14 -0700

    add maplestory styling to my buttons

---
+++ app/javascript/controllers/my_calculator_controller.js
@@ -0,0 +1,33 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+  }
+
+  initialize() {
+    this.value = ''
+  }
+
+  display() {
+    const screenElement = this.element.querySelector(`#calculator-screen`)
+
+    screenElement.innerHTML = this.value
+  }
+
+  append(event) {
+    const input = event.target.innerHTML
+
+   this.value += input
+
+   this.display()
+  }
+
+  equals() {
+    this.value = eval(this.value)
+
+    this.display()
+
+    this.value = ''
+  }
+}
+
